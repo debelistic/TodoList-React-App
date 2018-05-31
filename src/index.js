5f0a0ebe141564237cbc9660728e6c09ef3842d1
Victor Awotidebe <victorawotidebe@gmail.com>
2018-05-31 02:38:18 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import ReactDom from "react-dom";
+
+import "./index.css";
+
+import TodoList from "./TodoList";
+
+var destination = document.querySelector("#container");
+
+ReactDom.render(
+	<div>
+		<TodoList/>
+	</div>,
+
+	destination
+
+);
